870ee7965c7114cc22ebbdfccee698f121d2f43f
Adam Martin <adam.martin223@gmail.com>
2014-11-28 23:34:03 -0500

    Foreground canvas

--- connect4/application/views/match/board.php
+++ connect4/application/views/match/board.php
@@ -121,28 +121,14 @@
 	foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 	rtrim($fields_string, '&');
 
-	//open connection
-	$ch = curl_init();
-
-	//set the url, number of POST vars, POST data
-	curl_setopt($ch,CURLOPT_URL, $url);
-	curl_setopt($ch,CURLOPT_POST, count($fields));
-	curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-	//execute post
-	$result = curl_exec($ch);
-
-	echo "<div>"+$result+"</div>"
-
-	//close connection
-	curl_close($ch);
-
-
 	echo form_textarea('conversation');
 	
 	echo form_open();
 	echo form_input('msg');
 	echo form_submit('Send','Send');
+	echo form_submit('Send','Send');
+	echo form_submit('Send','Send');
+
 	echo form_close();
 	
 ?>

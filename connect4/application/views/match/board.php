e4034131dc95a9bf9d0c466529014811cafefa50
Adam Martin <adam.martin223@gmail.com>
2014-11-28 21:14:08 -0500

    Foreground canvas

--- connect4/application/views/match/board.php
+++ connect4/application/views/match/board.php
@@ -67,7 +67,7 @@
 			echo "Wating on " . $otherUser->login;
 	?>
 	</div>
-	<div class="gameArea" style="position:relative;">
+	<div class="gameArea" style="position:relative;" width="700" height="600">
 		<canvas class="board" id="frame" width="700" height="600" style=
 											"z-index: 1;
 											position:absolute;

15b68a256e951874e2e405ffe1cb3469bc0a7d3d
Adam Martin <adam.martin223@gmail.com>
2014-11-30 13:44:31 -0500

    Adding win checks

--- connect4/application/views/match/board.php
+++ connect4/application/views/match/board.php
@@ -56,217 +56,216 @@
 <button id="6" onclick="setCol('6')">SELECT PLAY</button>
 
 <script>
-		var otherUser = "<?= $otherUser->login ?>";
-		var user = "<?= $user->login ?>";
-		var status = "<?= $status ?>";
-		var side = "<?= $side ?>";
-		var other = (side == 1) ? 2 : 1;
-		var col = -1;
-		var colour1 = "red";
-		var bgrd_col = "#FFFF00";
-		var colour2 = "blue";
-		var game = {turn:1, 
-					board:
-					[[0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0]]};
+	var otherUser = "<?= $otherUser->login ?>";
+	var user = "<?= $user->login ?>";
+	var status = "<?= $status ?>";
+	var side = "<?= $side ?>";
+	var other = (side == 1) ? 2 : 1;
+	var col = -1;
+	var colour1 = "red";
+	var bgrd_col = "#FFFF00";
+	var colour2 = "blue";
+	var game = {turn:1, 
+				board:
+				[[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0],
+				[0,0,0,0,0,0,0]]};
 
-		//$(function(){
-			$('body').everyTime(2000,function(){
-					if (status == 'waiting') {
-						$.getJSON('<?= base_url() ?>arcade/checkInvitation',function(data, text, jqZHR){
-								if (data && data.status=='rejected') {
-									alert("Sorry, your invitation to play was declined!");
-									window.location.href = '<?= base_url() ?>arcade/index';
-								}
-								if (data && data.status=='accepted') {
-									status = 'playing';
-									$('#status').html('Playing ' + otherUser);
-								}
-								
-						});
-					}
-					var url = "<?= base_url() ?>board/getMsg";
-					$.getJSON(url, function (data,text,jqXHR){
-						if (data && data.status=='success') {
-							var conversation = $('[name=conversation]').val();
-							var msg = data.message;
-							if (msg!= null && msg.length > 0){
-								console.log("Received: " + msg);
-								$('[name=conversation]').val(conversation + "\n" + otherUser + ": " + msg);
-								console.log("setting play for " + other + " in col " + msg);
-								setPlay(parseInt(msg), other);
-								drawPlays();	
+	//$(function(){
+		$('body').everyTime(2000,function(){
+				if (status == 'waiting') {
+					$.getJSON('<?= base_url() ?>arcade/checkInvitation',function(data, text, jqZHR){
+							if (data && data.status=='rejected') {
+								alert("Sorry, your invitation to play was declined!");
+								window.location.href = '<?= base_url() ?>arcade/index';
 							}
-						}
+							if (data && data.status=='accepted') {
+								status = 'playing';
+								$('#status').html('Playing ' + otherUser);
+							}
+							
 					});
-					console.log("looping");
-			});
-
-			$('form').submit(function(event){
-				if (game.turn == side){
-					var msg = $('[name=msg]').val();
-					setPlay(parseInt(msg), side);
-					var arguments = $(this).serialize();
-					var url = "<?= base_url() ?>board/postMsg";
-					$.post(url,arguments, function (data,textStatus,jqXHR){
-							var conversation = $('[name=conversation]').val();
-							$('[name=conversation]').val(conversation + "\n" + user + ": " + msg);
-							});
-					$('[name=msg]').val("select play");
-					col = -1;
-					drawPlays();
-					return false;
-				} else {
-					alert("It is not your turn!");
-					event.preventDefault();
 				}
-			});	
-	//	});
-		var bgrd = document.getElementById("frame");
-		var bctx = bgrd.getContext("2d");
-		var moves = document.getElementById("plays");
-		var mctx = moves.getContext("2d");
-
-		bctx.fillStyle = bgrd_col;
-		bctx.fillRect(0, 0, bgrd.width, bgrd.height);	
-
-		moves.addEventListener('click', function(event){
-			var x = event.pageX - moves.offsetLeft;
-			var sel = ~~(x/110);
-			if (document.getElementById(sel).disabled != true){
-				setCol(sel);
-			}
+				var url = "<?= base_url() ?>board/getMsg";
+				$.getJSON(url, function (data,text,jqXHR){
+					if (data && data.status=='success') {
+						var conversation = $('[name=conversation]').val();
+						var msg = data.message;
+						if (msg!= null && msg.length > 0){
+							console.log("Received: " + msg);
+							$('[name=conversation]').val(conversation + "\n" + otherUser + ": " + msg);
+							console.log("setting play for " + other + " in col " + msg);
+							setPlay(parseInt(msg), other);
+							drawPlays();	
+						}
+					}
+				});
+				console.log("looping");
 		});
 
-		function drawPlays(){
-			console.log("-->Drawing");
-			var w = 110;
-			var h = 110;
-			var i,j;
-			for (i = 0; i < 7; i++){
-				for (j=0; j < 6; j++){
-					mctx.beginPath();
-					var x = i*w + w/2;
-					var y = j*h + h/2;
-					mctx.arc(x, y, 50, 2*Math.PI, false);
-					if (game.board[j][i] == 0){
-						mctx.fillStyle = (col == i) ? "grey" : "white";
-					} else if (game.board[j][i] == 1){
-						mctx.fillStyle = colour1;
-					} else {
-						mctx.fillStyle = colour2;
-					}
-					mctx.fill();
-				    mctx.lineWidth = 3;
-				    if (game.board[j][i] < 3){
-				    	mctx.strokeStyle = bgrd_col;
-				    } else {
-				    	mctx.strokeStyle = "#00ff00";
-				    }
-				    mctx.stroke();				
+		$('form').submit(function(event){
+			if (game.turn == side){
+				var msg = $('[name=msg]').val();
+				setPlay(parseInt(msg), side);
+				var arguments = $(this).serialize();
+				var url = "<?= base_url() ?>board/postMsg";
+				$.post(url,arguments, function (data,textStatus,jqXHR){
+						var conversation = $('[name=conversation]').val();
+						$('[name=conversation]').val(conversation + "\n" + user + ": " + msg);
+						});
+				$('[name=msg]').val("select play");
+				col = -1;
+				drawPlays();
+				return false;
+			} else {
+				alert("It is not your turn!");
+				event.preventDefault();
+			}
+		});	
+//	});
+	var bgrd = document.getElementById("frame");
+	var bctx = bgrd.getContext("2d");
+	var moves = document.getElementById("plays");
+	var mctx = moves.getContext("2d");
+
+	bctx.fillStyle = bgrd_col;
+	bctx.fillRect(0, 0, bgrd.width, bgrd.height);	
+
+	moves.addEventListener('click', function(event){
+		var x = event.pageX - moves.offsetLeft;
+		var sel = ~~(x/110);
+		if (document.getElementById(sel).disabled != true){
+			setCol(sel);
+		}
+	});
+
+	function drawPlays(){
+		console.log("-->Drawing");
+		var w = 110;
+		var h = 110;
+		var i,j;
+		for (i = 5; i < 7; i++){
+			for (j=5; j < 6; j++){
+				mctx.beginPath();
+				var x = i*w + w/2;
+				var y = j*h + h/2;
+				mctx.arc(x, y, 50, 2*Math.PI, false);
+				if (game.board[j][i] == 0){
+					mctx.fillStyle = (col == i) ? "grey" : "white";
+				} else if (game.board[j][i] == 1){
+					mctx.fillStyle = colour1;
+				} else {
+					mctx.fillStyle = colour2;
 				}
+				mctx.fill();
+			    mctx.lineWidth = 5;
+			    if (game.board[j][i] < 3){
+			    	mctx.strokeStyle = bgrd_col;
+			    } else {
+			    	mctx.strokeStyle = "#00ff00";
+			    }
+			    mctx.stroke();				
 			}
 		}
+	}
 
-		function setPlay(play, player){
-			console.log("-->Setting " + play);
-			if (play < 7 && play >= 0 && player == game.turn){
-				if (game.turn == 1){
-					game.turn = 2;
-				} else {
-					game.turn = 1;
-				}
-				for (i=5; i >= 0; i--){
-					if (game.board[i][play] == 0){
-						if (player == 1){
-							game.board[i][play] = 2
-						} else {
-							game.board[i][play] = 1
-						}
-						checkWin(i, play);
-						if (i == 0){
-							document.getElementById(play).disabled = true;
-						}
-						break;
+	function setPlay(play, player){
+		console.log("-->Setting " + play);
+		if (play < 7 && play >= 0 && player == game.turn){
+			if (game.turn == 1){
+				game.turn = 2;
+			} else {
+				game.turn = 1;
+			}
+			for (i=5; i >= 0; i--){
+				if (game.board[i][play] == 0){
+					if (player == 1){
+						game.board[i][play] = 2
+					} else {
+						game.board[i][play] = 1
 					}
+					checkWin(i, play);
+					if (i == 0){
+						document.getElementById(play).disabled = true;
+					}
+					break;
 				}
 			}
-			drawPlays();
 		}
 		drawPlays();
+	}
+	drawPlays();
 
-		function setCol (str) {
-			col = parseInt(str);
-		  	$('#msg').val(str);
-			drawPlays();
-		}
+	function setCol (str) {
+		col = parseInt(str);
+	  	$('#msg').val(str);
+		drawPlays();
+	}
 
-		function checkWin(row, col){
-			/*var max = 1;
-			var above = (col < 6) ? (col + 1) : col;
-			var below = (col > 0) ? (col - 1) : col;
-			var left = (row > 0) ? (row - 1) : row;
-			var right = (row < 5) ? (row + 1) : row;
+	function checkWin(row, col){
+		/*var max = 1;
+		var above = (col < 6) ? (col + 1) : col;
+		var below = (col > 0) ? (col - 1) : col;
+		var left = (row > 0) ? (row - 1) : row;
+		var right = (row < 5) ? (row + 1) : row;
 
-			var i = below;
-			var j = left;
+		var i = below;
+		var j = left;
 
-			for (i <= above; i++){
-				for (j <= right; j++){
-					if ()
-				}
-			}*/
+		for (i <= above; i++){
+			for (j <= right; j++){
+				if ()
+			}
+		}*/
 
-			for (var i = 0; i < 6; i++){
-				for (var j = 0; j < 7; j++){
-					//Cols
-					if (game.board[i][j] > 0){
-						if (i < 3 && game.board[i][j] == game.board[i+1][j] && 
-									game.board[i][j] == game.board[i+2][j] && 
-									game.board[i][j] == game.board[i+3][j]){
-							game.board[i][j] += 2;
-							game.board[i+1][j] += 2;
-							game.board[i+2][j] += 2;
-							game.board[i+3][j] += 2;
-							return true;
-						}
-						else if (i > 2 && game.board[i][j] == game.board[i-1][j] && 
-										game.board[i][j] == game.board[i-2][j] && 
-										game.board[i][j] == game.board[i-3][j]){
-							game.board[i][j] += 2;
-							game.board[i-1][j] += 2;
-							game.board[i-2][j] += 2;
-							game.board[i-3][j] += 2;
-							return true;
-						}
-						//Rows
-						if (j < 4 && game.board[i][j] == game.board[i][j+1] && 
-									game.board[i][j] == game.board[i][j+2] && 
-									game.board[i][j] == game.board[i][j+3]){
-							game.board[i][j] += 2;
-							game.board[i][j+1] += 2;
-							game.board[i][j+2] += 2;
-							game.board[i][j+3] += 2;
-							return true;
-						}
-						else if (j > 2 && game.board[i][j] == game.board[i][j-1] && 
-									game.board[i][j] == game.board[i][j-2] && 
-									game.board[i][j] == game.board[i][j-3]){
-							game.board[i][j] += 2;
-							game.board[i][j+1] += 2;
-							game.board[i][j+2] += 2;
-							game.board[i][j+3] += 2;
-							return true;
-						}
+		for (var i = 0; i < 6; i++){
+			for (var j = 0; j < 7; j++){
+				//Cols
+				if (game.board[i][j] > 0){
+					if (i < 3 && game.board[i][j] == game.board[i+1][j] && 
+								game.board[i][j] == game.board[i+2][j] && 
+								game.board[i][j] == game.board[i+3][j]){
+						game.board[i][j] += 2;
+						game.board[i+1][j] += 2;
+						game.board[i+2][j] += 2;
+						game.board[i+3][j] += 2;
+						game.turn = 0;
+					}
+					else if (i > 2 && game.board[i][j] == game.board[i-1][j] && 
+									game.board[i][j] == game.board[i-2][j] && 
+									game.board[i][j] == game.board[i-3][j]){
+						game.board[i][j] += 2;
+						game.board[i-1][j] += 2;
+						game.board[i-2][j] += 2;
+						game.board[i-3][j] += 2;
+						game.turn = 0;
+					}
+					//Rows
+					if (j < 4 && game.board[i][j] == game.board[i][j+1] && 
+								game.board[i][j] == game.board[i][j+2] && 
+								game.board[i][j] == game.board[i][j+3]){
+						game.board[i][j] += 2;
+						game.board[i][j+1] += 2;
+						game.board[i][j+2] += 2;
+						game.board[i][j+3] += 2;
+						game.turn = 0;
+					}
+					else if (j > 2 && game.board[i][j] == game.board[i][j-1] && 
+								game.board[i][j] == game.board[i][j-2] && 
+								game.board[i][j] == game.board[i][j-3]){
+						game.board[i][j] += 2;
+						game.board[i][j+1] += 2;
+						game.board[i][j+2] += 2;
+						game.board[i][j+3] += 2;
+						game.turn = 0;
 					}
 				}
 			}
-			return false;
 		}
+	}
 </script>
 
 </body>

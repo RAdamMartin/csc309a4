c4daf69777403b0f81756e9aa84b6a62cd08503d
Adam Martin <adam.martin223@gmail.com>
2014-12-05 18:39:06 -0500

    board alignment

--- connect4/application/views/match/board.php
+++ connect4/application/views/match/board.php
@@ -150,7 +150,7 @@
 		for (i = 0; i < 7; i++){
 			for (j=0; j < 6; j++){
 				mctx.beginPath();
-				var x = i*w + w/2 - 50;
+				var x = i*w + w/2 - 55;
 				var y = j*h + h/2;
 				mctx.arc(x, y, 50, 2*Math.PI, false);
 				if (game.board[j][i] == 0){

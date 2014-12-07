30373405716dd772372d533cd0908da0f057b5c1
Adam Martin <adam.martin223@gmail.com>
2014-12-07 01:54:31 -0500

    win check doesn't work

--- connect4/application/controllers/board.php
+++ connect4/application/controllers/board.php
@@ -107,7 +107,7 @@ class Board extends CI_Controller {
  			log_message('debug','[Winner'.$winner);
  			if ($winner > 0){
  				log_message('debug','[setting winner'.$winner);
- 				//$this->match_model->updateStatus($winner+1);
+ 				$this->match_model->updateStatus($winner+1);
  				log_message('debug','[set winner'.$winner);
  			}
  			$this->match_model->updateBoardState($match->id, serialize($matchHist));

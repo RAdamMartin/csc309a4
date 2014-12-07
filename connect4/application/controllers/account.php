bb500304aee80695b3d92ddb96655cb71ec42c0b
Adam Martin <adam.martin223@gmail.com>
2014-12-06 21:57:02 -0500

    Added secure image library

--- connect4/application/controllers/account.php
+++ connect4/application/controllers/account.php
@@ -76,7 +76,7 @@ class Account extends CI_Controller {
 	    	$this->form_validation->set_rules('first', 'First', "required");
 	    	$this->form_validation->set_rules('last', 'last', "required");
 	    	$this->form_validation->set_rules('email', 'Email', "required|is_unique[user.email]");
-	    	$inputCode = $this->input->post('imagecode');
+	    	$inputCode = $this->input->post('captcha_code');
 	    	include_once 'securimage/securimage.php';
 	    	$securimage = new Securimage();
 	    

d6c171ce65192715ed82cc80596a6d5bff19c8e3
Adam Martin <adam.martin223@gmail.com>
2014-12-06 23:23:44 -0500

    win check doesn't work

--- connect4/application/controllers/board.php
+++ connect4/application/controllers/board.php
@@ -193,7 +193,7 @@ function checkWin($matchHist){
 	}
 	for ($i =0 $i< 6; $i++){
 		for ($j=0; $j<7; $j++){
-			log_message('debug', '['.$i.']['.$j.'] : '.$p1[$i][$j].', '.$p2[$i][$j]);
+			//log_message('debug', '['.$i.']['.$j.'] : '.$p1[$i][$j].', '.$p2[$i][$j]);
 		}
 	}
 
